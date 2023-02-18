94a9301b4b003d3127f0d486e3712f3ecb522bb2
Delunico <delunico@sheridancollege.ca>
2023-02-17 19:06:03 -0500

    fix eslint target _blank error on build

--- src/components/google-drive/File.js
+++ src/components/google-drive/File.js
@@ -4,8 +4,8 @@ import { faFile } from '@fortawesome/free-solid-svg-icons'
 
 export default function File({ file }) {
     return (
-        <a href={file.url} target="_blank" className="btn btn-outline-dark text-truncate w-100">
-           <FontAwesomeIcon icon={faFile} className="mx-2"/>
+        <a href={file.url} rel="noreferrer" target="_blank" className="btn btn-outline-dark text-truncate w-100">
+            <FontAwesomeIcon icon={faFile} className="mx-2" />
             {file.name}
         </a>
     )
