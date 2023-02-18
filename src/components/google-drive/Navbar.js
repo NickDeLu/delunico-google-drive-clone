8abf5d9e07b8b27781c302918f6d8dc9827b2186
Delunico <delunico@sheridancollege.ca>
2023-02-17 19:12:29 -0500

    remove more eslint warnings

--- src/components/google-drive/Navbar.js
+++ src/components/google-drive/Navbar.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap' 
+import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import  logo  from '../../img/favicon.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
- 
+import logo from '../../img/favicon.png'
+
 export default function NavbarComponent() {
 
     return (
         <Navbar bg="light" expand="sm" >
             <Navbar.Brand as={Link} to="/" className="m-3 flex-grow-1">
-                <img src={logo}></img>Delunico Drive
+                <img src={logo} alt="logo"></img>Delunico Drive
             </Navbar.Brand>
             <Nav>
                 <Nav.Link as={Link} to="/user" className="m-3">
